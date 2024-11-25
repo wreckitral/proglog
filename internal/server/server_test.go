855fa9096caeb2e419482bf5cb3e1f7b98517d6e
wreckitral <defhanayasofhiea@gmail.com>
2024-11-25 21:31:38 +0700

    test: server

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -9,8 +9,9 @@ import (
 	"github.com/stretchr/testify/require"
 	api "github.com/wreckitral/proglog/api/v1"
 	"github.com/wreckitral/proglog/internal/log"
+	"github.com/wreckitral/proglog/internal/config"
+    "google.golang.org/grpc/credentials"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
 )
 
@@ -42,13 +43,32 @@ func setupTest(t *testing.T, fn func(*Config)) (
 ) {
     t.Helper()
 
-    l, err := net.Listen("tcp", ":0")
+    l, err := net.Listen("tcp", "127.0.0.1:0")
     require.NoError(t, err)
 
-    clientOptions := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-
-    cc, err := grpc.NewClient(l.Addr().String(), clientOptions...)
+    clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+        CAFile: config.CAFile,
+    })
     require.NoError(t, err)
+
+    clientCreds := credentials.NewTLS(clientTLSConfig)
+
+    cc, err := grpc.NewClient(
+        l.Addr().String(),
+        grpc.WithTransportCredentials(clientCreds),
+    )
+    require.NoError(t, err)
+
+    client = api.NewLogClient(cc)
+
+    serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+        CertFile: config.ServerCertFile,
+        KeyFile: config.ServerKeyFile,
+        CAFile: config.CAFile,
+        ServerAddress: l.Addr().String(),
+    })
+    require.NoError(t, err)
+    serverCreds := credentials.NewTLS(serverTLSConfig)
 
     dir, err := os.MkdirTemp("", "server-test")
     require.NoError(t, err)
@@ -59,25 +79,20 @@ func setupTest(t *testing.T, fn func(*Config)) (
     cfg = &Config{
         CommitLog: clog,
     }
-
     if fn != nil {
         fn(cfg)
     }
-
-    server, err := NewGRPCServer(cfg)
+    server, err := NewGRPCServer(cfg, grpc.Creds(serverCreds))
     require.NoError(t, err)
 
     go func() {
         server.Serve(l)
     }()
 
-    client = api.NewLogClient(cc)
-
     return client, cfg, func() {
         server.Stop()
         cc.Close()
         l.Close()
-        clog.Remove()
     }
 }
 
